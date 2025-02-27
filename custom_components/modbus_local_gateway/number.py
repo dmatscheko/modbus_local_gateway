e2c08009d608ad9ec0cdfb7dde03b9ae25786cce
David Matscheko <d@14.gy>
2025-02-27 17:50:12 +0100

    state now works

--- custom_components/modbus_local_gateway/number.py
+++ custom_components/modbus_local_gateway/number.py
@@ -83,7 +83,7 @@ class ModbusNumberEntity(ModbusCoordinatorEntity, NumberEntity):  # type: ignore
             await self.coordinator.client.write_data(self.coordinator_context, value)
 
     @property
-    def state(self) -> int | float:
+    def state(self) -> int | float | str:
         """Return the state of the entity, formatted based on register_multiplier."""
         if self._attr_native_value is None:
             return ""
